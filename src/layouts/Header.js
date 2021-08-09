cad02af2854153ca1e23295d01d7d9d13ece8131
ArcanumLibella <fiona.roux@hetic.net>
2021-08-09 11:57:01 +0200

    init: first commit

---
+++ src/layouts/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header () {
+  return (
+    <header className='p-4 bg-green-500 shadow-md'>
+      <h1 className='text-center text-white uppercase font-black'>React Songs</h1>
+    </header>
+  )
+}
