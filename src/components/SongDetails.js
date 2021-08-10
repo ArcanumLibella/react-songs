7a410eeeaccfa64ce5c8aaee2c63a17ae4657c3a
ArcanumLibella <fiona.roux@hetic.net>
2021-08-10 14:51:37 +0200

    feat: add SongDetails logic with redux

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const SongDetails = (props) => {
-    console.log(props)
+const SongDetails = ({song}) => {
+    if (!song) {
+        return <p className="">No song selected !</p>
+    }
+
     return (
         <div className="w-full mb-4 px-4 py-8 bg-green-100 rounded-md p-8">
             <h2 className="mb-8 font-black">SONGS DETAILS</h2>
+            <p className="font-bold">Title : {song.title}</p>
+            <p className="font-bold">Duration : {song.duration}</p>
         </div>
     )
 }
--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -3,36 +3,6 @@ import { connect } from 'react-redux'
 import { selectSong } from '../actions'
 // import { Song } from './'
 
-// const SongsList = (songs) => {
-//     console.log(songs)
-
-//     const renderList = () => {
-//         return songs.songs.map(song => {
-//             return (
-//                 <div 
-//                     key={song.title}
-//                     className="flex justify-between items-center p-4 my-2 bg-green-300 rounded-md"
-//                 >
-//                     <h3 className="font-bold text-xs md:text-sm">{song.title}</h3>
-//                     <button 
-//                         onClick={() => selectSong(song)}
-//                         className="py-2 px-4 rounded-md bg-green-500 hover:bg-green-700 text-xs md:text-sm font-bold text-white transition-all"
-//                     >
-//                         Select
-//                     </button>
-//                 </div>
-//             )
-//         })    
-//     } 
-
-//     return (
-//         <div className="w-full mb-4 md:mx-4 px-4 py-8 rounded-md bg-green-100">
-//             <h2 className="mb-8 font-black">SONGS LIST</h2>
-//             {renderList()}
-//         </div>
-//     )
-// }
-
 class SongsList extends Component {
     renderList() {
         return this.props.songs.map(song => {
@@ -62,6 +32,37 @@ class SongsList extends Component {
         );
     }
   }
+
+  // FIXME: Functional component doesn't work
+// const SongsList = (songs) => {
+//     console.log(songs)
+
+//     const renderList = () => {
+//         return songs.songs.map(song => {
+//             return (
+//                 <div 
+//                     key={song.title}
+//                     className="flex justify-between items-center p-4 my-2 bg-green-300 rounded-md"
+//                 >
+//                     <h3 className="font-bold text-xs md:text-sm">{song.title}</h3>
+//                     <button 
+//                         onClick={() => selectSong(song)}
+//                         className="py-2 px-4 rounded-md bg-green-500 hover:bg-green-700 text-xs md:text-sm font-bold text-white transition-all"
+//                     >
+//                         Select
+//                     </button>
+//                 </div>
+//             )
+//         })    
+//     } 
+
+//     return (
+//         <div className="w-full mb-4 md:mx-4 px-4 py-8 rounded-md bg-green-100">
+//             <h2 className="mb-8 font-black">SONGS LIST</h2>
+//             {renderList()}
+//         </div>
+//     )
+// }
 
 const mapStateToProps = (state) => {
     console.log(state)
