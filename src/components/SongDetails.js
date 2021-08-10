1abf6d5eecef5cc4d6d8cfa9f8fe9a867260af5f
ArcanumLibella <fiona.roux@hetic.net>
2021-08-10 14:31:34 +0200

    feat: add Songs details logic

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-export default function SongDetails() {
+const SongDetails = (props) => {
+    console.log(props)
     return (
         <div className="w-full mb-4 px-4 py-8 bg-green-100 rounded-md p-8">
             <h2 className="mb-8 font-black">SONGS DETAILS</h2>
         </div>
     )
 }
+
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetails)
