0500b8dba8b95d7e6cc684110e028fbbe63b87d7
ArcanumLibella <fiona.roux@hetic.net>
2021-08-10 15:22:27 +0200

    fix: change class component to functional component

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -3,7 +3,12 @@ import { connect } from 'react-redux'
 
 const SongDetails = ({song}) => {
     if (!song) {
-        return <p className="">No song selected !</p>
+        return (
+            <div className="w-full mb-4 px-4 py-8 bg-green-100 rounded-md p-8">
+                <h2 className="mb-8 font-black">SONGS DETAILS</h2>
+                <p className="">No song selected !</p>
+            </div>
+        )
     }
 
     return (
